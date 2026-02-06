08a8b866f30f8663dfd8017185b63b0f97e6e9c5
purnimashrestha <sthapurnima90@gmail.com>
2026-02-06 09:52:12 +0545

    second

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import type { Task } from "@/types/tasks";
+import TaskItems from "./TaskItems";
+interface Props {
+  tasks: Task[];
+  onToggle: (id:number) => void;
+  onDelete: (id: number) => void;
+}
+
+function TaskList({ tasks, onToggle, onDelete }: Props) {
+  return (
+    <div>
+      {tasks.map((t) => (
+      <TaskItems 
+      onToggle={onToggle}
+      onDelete={onDelete}
+       id={t.id} 
+       completed={t.completed}
+       title={t.title}
+       />
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
